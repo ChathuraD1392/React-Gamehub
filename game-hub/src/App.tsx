20a9c58ab2f036b596bfb9934c7e37567dac71d4
ChathuraD1392 <chathuradilshan1392@gmail.com>
2024-09-09 11:21:06 +0530

    setup responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,9 +1,25 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <Grid
+        templateAreas={`"nav nav"
+        "aside main"
+        `}
+        gap="1"
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <GridItem area="aside" bg="tomato">
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
